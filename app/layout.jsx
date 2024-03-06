54e475d7c632491d02af0233aaa1d7a1dfcb0347
ianandparmar <sameeranandparmar@gmail.com>
2024-03-06 17:36:54 +0530

    Layout, Homepage & Metadata

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@/assets/styles/global.css';
+
+export const metadata = {
+       title: 'PropertyPulse | Find the Perfect Rental',
+       description: 'Find your dream rental property',
+       keyword: 'oyo, rental, find rentals, find properties',
+}
+const MainLayout = ({children}) => {
+       return (
+              <html lang='en'>
+                     <body>
+                            <div>{children}</div>
+                     </body>
+              </html>
+       )
+}
+
+export default MainLayout
