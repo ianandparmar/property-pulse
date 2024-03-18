307ec1a4d69eb610d52df7ffc1b27e489e1488c7
ianandparmar <sameeranandparmar@gmail.com>
2024-03-18 14:29:34 +0530

    Properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,23 @@
-import Link from "next/link";
-const PropertiesPage = () => {
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
+const propertiespage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">PropertiesPage</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found!</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              // <div>{property.name}</div>
+              <PropertyCard key={property._id} property={property.name} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
-export default PropertiesPage;
+export default propertiespage;
